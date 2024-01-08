2b6052a39bb7b4c9083bba25b29b7d7fdeec8730
Usmanwasif123 <usmanwasif8@gmail.com>
2024-01-08 20:44:55 +0500

    contact comp changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import '../assets/wrapper/Contact.css';
+import { Grid, styled, Paper } from '@mui/material';
 
 const Contact = () => {
+  const Item = styled(Paper)(({ theme }) => ({
+    padding: theme.spacing(25),
+    textAlign: 'center',
+    background: '#333',
+    borderRadius: '20px',
+    border: '2px solid #1932d1',
+    position: 'absolute',
+    height: '40px',
+    width: '50px',
+    left: '2%',
+    top: '-700%',
+}));
+
   return (
     <div className='contact-container'>
-      Contact
+      <img src='https://i.ibb.co/chrdsWb/contact.png' alt='contact-bg'/>
+      <Grid container spacing={8}>
+        <Grid item xs={6} md={2} className='contact-grid1'>
+        <Item className='contact-item1'>
+
+            <h2>CONSTANT CLIENT CO-ORDINATION</h2>
+        </Item>
+        </Grid>
+        <Grid item xs={6} md={2} className='contact-grid2'>
+        <Item className='contact-item2'>
+            
+            <h2>CONSTANT CLIENT CO-ORDINATION</h2>
+        </Item>
+        </Grid>
+        </Grid>
     </div>
   )
 }
