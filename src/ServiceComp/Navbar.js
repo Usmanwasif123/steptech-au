99da1e0be99f5e96ce14ffd455a2f8ca3e707eb1
Usmanwasif123 <usmanwasif8@gmail.com>
2024-01-31 18:32:10 +0500

    changes added in two pages

--- src/ServiceComp/Navbar.js
+++ src/ServiceComp/Navbar.js
@@ -1,12 +1,50 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import '../assets/ServiceWrapper/Navbar.css';
 import {Link} from 'react-router-dom';
 import {FaBars, FaTimes} from 'react-icons/fa';
+import {KeyboardArrowDownOutlined, Clear }from '@mui/icons-material';
 
 const Navbar = () => {
 
 
   const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const [closeTimeout, setCloseTimeout] = useState(null);
+
+  const handleMouseEnter = () => {
+    console.log('Mouse enter');
+    clearTimeout(closeTimeout); // Clear any existing timeout to close the dropdown
+    setDropdownOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    // Set a timeout to close the dropdown after a delay
+    const timeoutId = setTimeout(() => {
+      setDropdownOpen(false);
+    }, 300); // You can adjust the delay time (in milliseconds) as needed
+    setCloseTimeout(timeoutId);
+  };
+
+  const handleDropdownMouseEnter = () => {
+    clearTimeout(closeTimeout); // Clear the timeout when entering the dropdown content
+  };
+
+  const handleDropdownMouseLeave = () => {
+    // Set a timeout to close the dropdown after a delay
+    const timeoutId = setTimeout(() => {
+      setDropdownOpen(false);
+    }, 300); // You can adjust the delay time (in milliseconds) as needed
+    setCloseTimeout(timeoutId);
+  };
+
+  useEffect(() => {
+    // Clear the timeout when the component is unmounted
+    return () => {
+      clearTimeout(closeTimeout);
+    };
+  }, [closeTimeout]);
 
   const toggleSidebar = () => {
     console.log('Toggle Sidebar clicked');
@@ -23,13 +61,32 @@ const Navbar = () => {
           </button>
         <nav>
         <ul className='nav-items'>
-            <li><a href='https://steptech.com.au/'>home</a></li>
-            <li><a className='about'>about us</a></li>
-            <li><a className='service'>services</a></li>
-            <li><a className='portfolio'>portfolio</a></li>
-            <li><a className='blog'>blog</a></li>
-            <hr></hr>
-            <Link to='/get-a-quote' className='quote-btn'>get a quote </Link>
+        <li><Link to='/'>home</Link></li>
+            <li><Link to='/about-us' className='about'>about us</Link></li>
+            <li className='dropdown' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+           <KeyboardArrowDownOutlined style={{position:'absolute', zIndex: 1, fontSize: '25px', top: '290%', left: '73.4px', color: "#fff"}}/>
+            <Link to='/service' className='service' style={{marginTop: '6px'}}>services</Link>
+            {isDropdownOpen && 
+            <div className='dropdown-content' ref={dropdownRef} onMouseEnter={handleDropdownMouseEnter} onMouseLeave={handleDropdownMouseLeave}>
+             <li><Link to='/web-design-and-development' className='nav-service1'>web design & development</Link></li>
+             <li><Link to='/app-development' className='nav-service2'>Mobile UI/UX Design & development</Link></li>
+             <li><Link to='/game-design-and-development' className='nav-service3'>game design and development</Link></li>
+             <li><Link to='/2D/3D-Animation' className='nav-service4'>2d/3d animation</Link></li>
+             <li><Link to='/graphic-designing' className='nav-service5'>graphic designing</Link></li>
+             <li><Link to='/block-chain-development' className='nav-service6'>block chain development</Link></li>
+             <li><Link to='/nft' className='nav-service7'>nft</Link></li>
+             <li><Link to='/artificial-intelligence-development' className='nav-service8'>artificial intelligence</Link></li>
+             <li><Link to='/software-development' className='nav-service9'>software development</Link></li>
+             <li><Link to='/search-engine-optimization' className='nav-service10'>search engine optimization</Link></li>
+             <li><Link to='/social-media-management' className='nav-service11'>social media marketing</Link></li>
+             <li><Link to='/digital-marketing' className='nav-service12'>digital marketing</Link></li>
+             <li><Link to='/ecommerce' className='nav-service13'>e-commerce</Link></li>
+            </div>
+          }
+          </li>
+            <li><Link to='/portfolio' className='portfolio'>portfolio</Link></li>
+            <li><Link to='/blog' className='blog'>blog</Link></li>
+            <button><a href='tel:+61410025346' class='call-btn'>+61 410 025 346</a></button>
             <button>
               <FaTimes/>
             </button>
