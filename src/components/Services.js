fdf6ce02b6cc2a65ce7230be2671211aa99a2f01
Usmanwasif123 <usmanwasif8@gmail.com>
2024-03-13 12:38:46 +0500

    images changes in the service comp

--- src/components/Services.js
+++ src/components/Services.js
@@ -28,17 +28,17 @@ const Services = () => {
         <Button style={{background: 'linear-gradient(256.4deg, #6347AA 7.85%, #233F81 107.97%)', border: 'none', borderRadius: '0.5rem',  color: '#fff', fontSize: '1.5rem', display: 'flex', textAlign: 'centre', justifyContent: 'center',  marginTop: '2rem', padding: '0.5rem', marginLeft: '3rem', width: '15rem'}}>Explore More</Button>
         <div style={{ marginTop: '-26rem', marginLeft: '25rem', position: 'absolute', display: 'inline-block', width: '50rem'}}> {/* Adjust margin-top as needed */}
           <Card style={{ background: '#fff', height: '33rem', width: '50rem', marginLeft: '7rem', zIndex: '0', overflow: 'hidden'}}>
-          <img style={{height: 'auto', width: '100%'}} src='https://i.ibb.co/txSYFYv/discover-and-planning.jpg' alt=''/>
+          <img style={{height: 'auto', width: '100%'}} src='https://i.ibb.co/NTPLxtM/1-banner.jpg' alt=''/>
           </Card>
         </div>
         <div style={{ marginTop: '8rem', marginLeft: '25rem', position: 'absolute', display: 'inline-block', zIndex: '1', width: '50rem', transition: 'transform 0.5s ease', transform: `translateX(-${scrollPosition / 10}px) translateX(${scrollPosition / -6}px)`}}> {/* Adjust margin-top as needed */}
           <Card className='slideLeft' style={{ background: '#0000ff', height: '31rem', width: '50rem', marginLeft: '5rem', marginTop: '-31rem',  overflow: 'hidden' }}>
-          <img style={{height: 'auto', width: '100%'}} src='https://i.ibb.co/HGG61Gy/design-prototyping.jpg' alt=''/>
+          <img style={{height: 'auto', width: '100%'}} src='https://i.ibb.co/BZ9bR3s/2-banner.jpg' alt=''/>
           </Card>
         </div>
         <div style={{ marginTop: '10rem', marginLeft: '25rem', position: 'absolute', display: 'inline-block',zIndex: '2', width: '50rem', transition: 'transform 0.5s ease', transform: `translateX(-${scrollPosition / 5}px) translateX(${scrollPosition / -6}px) `}}> {/* Adjust margin-top as needed */}
           <Card className='slideLeft' style={{ background: '#0044ff', height: '29rem', width: '50rem', marginLeft: '3rem', marginTop: '-29rem',  overflow: 'hidden' }}>
-            <img style={{height: 'auto', width: '100%'}} src='https://i.ibb.co/txSYFYv/discover-and-planning.jpg' alt=''/>
+            <img style={{height: 'auto', width: '100%'}} src='https://i.ibb.co/LZxM3cq/3-banner.jpg' alt=''/>
           </Card>
         </div>
       </Col>
